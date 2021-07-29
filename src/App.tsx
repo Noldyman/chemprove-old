cd506f41a44bf417c652ff57f9c97913e2c68c23
Noud Verstijnen <noud.verstijnen@gmail.com>
2021-07-29 18:19:17 +0200

    added under construction banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { ThemeProvider } from '@material-ui/styles'
 import { CssBaseline } from '@material-ui/core'
+import { Alert } from '@material-ui/lab'
 import { AppBar } from './components/general/AppBar'
 import { lightTheme, darkTheme } from './themesAndStyles/themes'
 import { NmrResiduePage } from './components/nmr/NmrResiduePage'
@@ -17,6 +18,14 @@ function App() {
           onThemeToggle={() => setIsThemeDark((prevValue) => !prevValue)}
         />
         <div className="content">
+          <Alert
+            severity="error"
+            variant="outlined"
+            style={{ marginTop: '35px' }}
+          >
+            This website is not yet finished and is still under construction.
+            Keep this in mind when using the site.
+          </Alert>
           <NmrResiduePage />
         </div>
       </div>
