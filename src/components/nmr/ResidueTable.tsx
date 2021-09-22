5bb5f7b5035288ad5833486213156e6ab92ddd12
Noud Verstijnen <noud.verstijnen@gmail.com>
2021-09-22 20:24:27 +0200

    optimized scaling for new UI

--- src/components/nmr/ResidueTable.tsx
+++ src/components/nmr/ResidueTable.tsx
@@ -16,10 +16,13 @@ import {
 const useStyles = makeStyles({
   autocomplete: {
     width: 300,
-    '@media (max-width: 1300px)': {
+    '@media (max-width: 1100px)': {
+      width: 280,
+    },
+    '@media (max-width: 900px)': {
       width: 250,
     },
-    '@media (max-width: 1205px)': {
+    '@media (max-width: 800px)': {
       width: 220,
     },
   },
